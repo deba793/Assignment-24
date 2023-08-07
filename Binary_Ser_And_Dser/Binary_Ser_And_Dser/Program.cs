922075b2c04882631bc33ae94d83e690e469008e
deba793 <debasisrout793@gmail.com>
2023-08-07 15:45:37 +0530

    Assignment-24

---
+++ Binary_Ser_And_Dser/Binary_Ser_And_Dser/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeLib;
+
+namespace Binary_Ser_And_Dser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Id = 19,
+                FirstName = "Debendra",
+                LastName = "Rout",
+                Salary = 60000.00
+            };
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("E:\\Day-21\\Assignment-21\\Binary_Ser_And_Dser\\employee.bin", FileMode.Create))
+            {
+                formatter.Serialize(fs, employee);
+                Console.WriteLine("Created and Serialized");
+                Console.WriteLine(employee.Id);
+                Console.WriteLine(employee.FirstName);
+                Console.WriteLine(employee.LastName);
+                Console.WriteLine(employee.Salary);
+
+            }
+            using (FileStream fs = new FileStream("E:\\Day-21\\Assignment-21\\Binary_Ser_And_Dser\\employee.bin", FileMode.Open))
+            {
+                Employee deserialize = (Employee)formatter.Deserialize(fs);
+                Console.WriteLine("De-Serialized");
+                Console.WriteLine(deserialize.Id);
+                Console.WriteLine(deserialize.FirstName);
+                Console.WriteLine(deserialize.LastName);
+                Console.WriteLine(deserialize.Salary);
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ XML_SER_DSER/XML_SER_DSER/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using EmployeeLib;
+
+namespace XML_SER_DSER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Id = 16,
+                FirstName = "Debendra",
+                LastName = "Rout",
+                Salary = 50000.00
+            };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+            using (TextWriter writer = new StreamWriter("E:\\Day-21\\Assignment-21\\XML_SER_DSER\\employee.xml"))
+            {
+                serializer.Serialize(writer, employee);
+                Console.WriteLine("De-Serialization");
+                Console.WriteLine($"Id: {employee.Id}");
+                Console.WriteLine($"First Name: {employee.FirstName}, Last Name: {employee.LastName}");
+                Console.WriteLine($"Salary: {employee.Salary}");
+            }
+            using (TextReader reader = new StreamReader("E:\\Day-21\\Assignment-21\\XML_SER_DSER\\employee.xml"))
+            {
+                Employee deserialize = (Employee)serializer.Deserialize(reader);
+                Console.WriteLine("De-Serialization");
+                Console.WriteLine($"Id: {deserialize.Id}");
+                Console.WriteLine($"First Name: {deserialize.FirstName}, Last Name: {deserialize.LastName}");
+                Console.WriteLine($"Salary: {deserialize.Salary}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
